273fc197b7bc493f55a93d39ba10e6f870a09844
unstory <2075218138@qq.com>
2020-09-07 20:26:28 +0800

    添加全排列算法

---
+++ n_quenn.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+using namespace std;
+
+const int MAXN = 11;
+int hashTable[MAXN] = {false};
+int P[MAXN] = {0};
+int n = 5;
+int count = 0;
+
+
+void generateP(int index){
+    if(index == n + 1){
+        count ++;
+        for(int i=1; i<=n; i++){
+            printf("%d", P[i]);
+        }
+        printf("%s", "----------\n");
+    }
+    for(int x=1; x <= n; x ++){
+        if(hashTable[x] == false){
+            P[index] = x;
+            hashTable[x] = true;
+            generateP(index + 1);
+            hashTable[x] = false;
+        }
+    }
+}
+
+int main(){
+    generateP(1);
+    printf("%d", count);
+    return 0;
+}
+
